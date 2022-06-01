392fd4717101522669d1567b9d98de8c27363337
igorbroza <igorbrozasantos127@gmail.com>
2022-05-31 22:28:54 -0300

    Atividade 1 - bug

---
+++ atividade 1/php/arrays02.php
@@ -0,0 +1,17 @@
+<?php
+	// Array: Definição Explícita (com chave)
+	$var = array(	"Maria"	=> 25, 
+				"João"	=> 44,
+				"José"	=> 12,
+				"Neusa"	=> 73,
+				"Neusa"	=> 65,
+			);
+
+	foreach ($var as $chave => $valor) {
+		echo "$chave: $valor<br>";
+	}
+	echo "<br>";
+	print_r($var);
+?>
+
+
