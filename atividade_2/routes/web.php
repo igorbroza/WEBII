4cb6ec5e66fe36655736545e5dbc16aa529654c8
igorbroza <igorbrozasantos127@gmail.com>
2022-06-13 22:09:38 -0300

    ativ 2

---
+++ atividade_2/routes/web.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::prefix('/aluno')->group(function() {
+
+    Route::get('/', function (){
+        $dados = array(
+            "Igor Broza",
+            "Gil Eduardo",
+            "Murilo Amancio",
+            "Lucas Kayure",
+            "Daph Alves"
+            );
+
+        $alunos = "<ul>";
+
+        $cont = 0;
+
+        foreach($dados as $nome) {
+            $alunos .= "<li>$nome</li>";
+            $cont++;
+        }
+
+        return $alunos;
+    });
+
+    Route::get('/limite/{total}', function($total) {
+
+        $dados = array(
+            "Igor Broza",
+            "Gil Eduardo",
+            "Murilo Amancio",
+            "Lucas Kayure",
+            "Daph Alves"
+            );
+
+        $alunos = "<ul>";
+
+        $cont = 0;
+
+        foreach($dados as $nome) {
+            $alunos .= "<li>$nome</li>";
+            $cont++;
+            if($cont >= $total) break;
+        }
+
+    return $alunos;
+    });
+});
