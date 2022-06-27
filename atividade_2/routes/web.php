bd912f31d186e4d96fdea8ece6a3154e30d872ee
igorbroza <igorbrozasantos127@gmail.com>
2022-06-27 20:01:18 -0300

    atividade 3 pronta

--- atividade_2/routes/web.php
+++ atividade_2/routes/web.php
@@ -63,22 +63,123 @@ Route::prefix('/aluno')->group(function() {
     return $alunos;
     });
 
-    Route::get('/matricula/{matricula}', function ($matricula){
+    Route::get('/matricula/{matricula}', function($matricula){
         $dados = array(
-            "Igor Broza",
-            "Gil Eduardo",
-            "Murilo Amancio",
-            "Lucas Kayure",
-            "Daph Alves"
-            );
+            array("matricula" => 1, "nome" => "Igor Broza", "nota" => 2),
+            array("matricula" => 2, "nome" => "Gil Eduardo", "nota" => 2),
+            array("matricula" => 3, "nome" => "Murilo Amancio", "nota" => 5),
+            array("matricula" => 4, "nome" => "Lucas Kayure", "nota" => 2),
+            array("matricula" => 5, "nome" => "Daph Alves", "nota" => 2),
+        );
+
+        $alunos = "<ul>";
+
+        foreach($dados as $arr) {
+            if($arr['matricula'] == $matricula){
+            $alunos .= "<li>".$arr['nome']."</li>";
+            }
+        }
+
+        return $alunos;
+    });
+
+    Route::get('/nome/{nome}', function($nome){
+        $dados = array(
+            array("matricula" => 1, "nome" => "Igor Broza", "nota" => 2),
+            array("matricula" => 2, "nome" => "Gil Eduardo", "nota" => 2),
+            array("matricula" => 3, "nome" => "Murilo Amancio", "nota" => 5),
+            array("matricula" => 4, "nome" => "Lucas Kayure", "nota" => 2),
+            array("matricula" => 5, "nome" => "Daph Alves", "nota" => 2),
+        );
+
+        $alunos = "<ul>";
+
+        foreach($dados as $arr) {
+            if($arr['nome'] == $nome){
+            $alunos .= "<li>".$arr['nome']."</li>";
+            }
+        }
+
+
+        return $alunos;
+    });
+});
+
+Route::prefix('/nota')->group(function(){
+
+    Route::get('/', function(){
+        $dados = array(
+            array("matricula" => 1, "nome" => "Igor Broza", "nota" => 2),
+            array("matricula" => 2, "nome" => "Gil Eduardo", "nota" => 2),
+            array("matricula" => 3, "nome" => "Murilo Amancio", "nota" => 5),
+            array("matricula" => 4, "nome" => "Lucas Kayure", "nota" => 2),
+            array("matricula" => 5, "nome" => "Daph Alves", "nota" => 2),
+        );
+
+        $alunos = "<ul>";
 
         $cont = 0;
 
-        foreach($dados as $nome) {
-            $alunos .= "<li>$nome</li>";
+        foreach($dados as $arr) {
+            $alunos .= "<li>".$arr['nota']."</li>";
             $cont++;
         }
 
         return $alunos;
     });
+
+
+
+    Route::get('/limite/{total}', function($total){
+        $dados = array(
+            array("matricula" => 1, "nome" => "Igor Broza", "nota" => 2),
+            array("matricula" => 2, "nome" => "Gil Eduardo", "nota" => 2),
+            array("matricula" => 3, "nome" => "Murilo Amancio", "nota" => 2),
+            array("matricula" => 4, "nome" => "Lucas Kayure", "nota" => 2),
+            array("matricula" => 5, "nome" => "Daph Alves", "nota" => 2),
+        );
+
+        $alunos = "<ul>";
+
+        $cont = 0;
+
+        foreach($dados as $arr) {
+            $alunos .= "<li>".$arr['nota']."</li>";
+            $cont++;
+            if($cont >= $total) break;
+        }
+
+    return $alunos;
+    });
+
+    Route::get('/lancar/{nota}/{matricula}/{nome?}', function($nota, $matricula, $nome=null){
+        $dados = array(
+            array("matricula" => 1, "nome" => "Igor Broza", "nota" => 2),
+            array("matricula" => 2, "nome" => "Gil Eduardo", "nota" => 2),
+            array("matricula" => 3, "nome" => "Murilo Amancio", "nota" => 2),
+            array("matricula" => 4, "nome" => "Lucas Kayure", "nota" => 2),
+            array("matricula" => 5, "nome" => "Daph Alves", "nota" => 2),
+        );
+
+        $indice = array_search($matricula, array_column($dados, 'matricula'));
+
+
+        $dados[$indice]['nota'] = $nota;
+
+
+        // Armazena os dados do cliente para o índice obtido
+        $dados = $aux[$indice];
+
+        $alunos = "<ul>";
+
+        $cont = 0;
+
+        foreach($dados as $arr) {
+            $alunos .= "<li>".$arr['nota']."</li>";
+            $cont++;
+        }
+
+        return $alunos;
+    });
+
 });
